fff17aef5ad465eb909fde4d222b14c51791d592
DESKTOP-24ONTIT\Admin <hoanghoang360@gmail.com>
2018-08-01 20:38:46 +0700

    update layout

--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -1,15 +1,93 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+declare var $: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  current: any;
+  prevRating: any;
+  nextRating: any;
+  constructor() {
 
-  constructor() { }
-
-  ngOnInit() {
   }
 
+  ngOnInit() {
+
+  }
+  ngAfterViewInit(): void {
+    $('.hero-area-slider').owlCarousel({
+      loop: true,
+      dots: true,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      nav: false,
+      items: 1,
+      responsive: {
+        992: {
+          dots: false,
+        }
+      }
+    });
+    $('.hero-area-slider').on('changed.owl.carousel', function (property) {
+      this.current = property.item.index;
+      this.prevRating = $(property.target).find('.owl-item').eq(this.current).prev().find('.hero-area-slide').html();
+      this.nextRating = $(property.target).find('.owl-item').eq(this.current).next().find('.hero-area-slide').html();
+      $('.thumb-prev .hero-area-slide').html(this.prevRating);
+      $('.thumb-next .hero-area-slide').html(this.nextRating);
+    });
+    $('.thumb-next').on('click', function () {
+      $('.hero-area-slider').trigger('next.owl.carousel', [300]);
+      return false;
+    });
+    $('.thumb-prev').on('click', function () {
+      $('.hero-area-slider').trigger('prev.owl.carousel', [300]);
+      return false;
+    });
+    $('.news-slider').owlCarousel({
+      loop: true,
+      dots: true,
+      autoplay: false,
+      autoplayTimeout: 4000,
+      nav: false,
+      items: 1,
+      responsive: {
+        992: {
+          dots: false,
+        }
+      }
+    });
+    $('.news-slider').on('changed.owl.carousel', function (property) {
+      this.current = property.item.index;
+      this.prevRating = $(property.target).find('.owl-item').eq(this.current).prev().find('.single-news').html();
+      this.nextRating = $(property.target).find('.owl-item').eq(this.current).next().find('.single-news').html();
+      $('.news-prev .single-news').html(this.prevRating);
+      $('.news-next .single-news').html(this.nextRating);
+    });
+    $('.news-next').on('click', function () {
+      $('.news-slider').trigger('next.owl.carousel', [300]);
+      return false;
+    });
+    $('.news-prev').on('click', function () {
+      $('.news-slider').trigger('prev.owl.carousel', [300]);
+      return false;
+    });
+    // $('.portfolio-item').isotope();
+    // $('.portfolio-menu li').on('click', function () {
+    //   $('.portfolio-menu li').removeClass('active');
+    //   $(this).addClass('active');
+    //   this.selector = $(this).attr('data-filter');
+    //   $('.portfolio-item').isotope({
+    //     filter: this.selector
+    //   });
+    // });
+
+    /*----------------------------
+      START - Preloader
+      ------------------------------ */
+    $(window).load(function () {
+      $('#preloader').fadeOut(500);
+    });
+  }
 }
