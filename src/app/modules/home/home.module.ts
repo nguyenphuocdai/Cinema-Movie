91aafd779779cb8759f7bfe37f7c150be795fbd2
DESKTOP-24ONTIT\Admin <hoanghoang360@gmail.com>
2018-08-02 00:02:40 +0700

    update layout

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,27 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
-import { ChannelsComponent } from './components/channels/channels.component';
-import { PopularVideoComponent } from './components/popular-video/popular-video.component';
-import { SliderComponent } from './components/slider/slider.component';
-import { SwiperModule } from 'ngx-useful-swiper';
 
 @NgModule({
   imports: [
-    CommonModule,
-    SwiperModule
+    CommonModule
+
   ],
   declarations: [
-    HomeComponent,
-    ChannelsComponent,
-    PopularVideoComponent,
-    SliderComponent
+    HomeComponent
   ],
   exports: [
     HomeComponent,
-    PopularVideoComponent,
-    ChannelsComponent,
-    SwiperModule
   ]
 })
 export class HomeModule { }
