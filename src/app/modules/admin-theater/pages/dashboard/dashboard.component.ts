a38b11f87ecbe7eba6b63574f723a48e7b128b87
DESKTOP-24ONTIT\Admin <hoanghoang360@gmail.com>
2018-08-21 00:07:43 +0700

    save script loader dashboard

--- src/app/modules/admin-theater/pages/dashboard/dashboard.component.ts
+++ src/app/modules/admin-theater/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ScriptService } from 'ngx-script-loader';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private scriptService: ScriptService, ) { }
 
   ngOnInit() {
+    this.scriptService.loadScript('../../../../../assets/js/plugin/chartist/plugin/chartist-plugin-tooltip.min.js').subscribe(() => {
+    }, (error) => {
+      console.log('Failed to load script chartist-plugin-tooltip js');
+    });
+    this.scriptService.loadScript('../../../../../assets/js/plugin/chartist/chartist.min.js').subscribe(() => {
+    }, (error) => {
+      console.log('Failed to load script chartist js');
+    });
+    this.scriptService.loadScript('../../../../../assets/js/demo.js').subscribe(() => {
+    }, (error) => {
+      console.log('Failed to load script chartist js');
+    });
   }
 
 }
